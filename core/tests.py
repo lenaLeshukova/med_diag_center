f028ddba69a8cf09e934a037ff672ddcad937149
Lena Leshukova <lena000183@gmail.com>
2026-09-26 02:30:33 +0300

    test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Feedback
 
-# Create your tests here.
+class CoreViewsTestCase(TestCase):
+    def test_index_page_status_code(self):
+        """Проверка доступности главной страницы."""
+        response = self.client.get(reverse('core:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/index.html')
+
+    def test_about_page_status_code(self):
+        """Проверка доступности страницы 'О компании'."""
+        response = self.client.get(reverse('core:about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_feedback_form_submission(self):
+        """Проверка отправки формы обратной связи."""
+        data = {
+            'name': 'Тест Тестович',
+            'email': 'test@example.com',
+            'phone': '+79991112233',
+            'message': 'Привет, это тестовое сообщение!'
+        }
+        response = self.client.post(reverse('core:index'), data=data)
+        # После успешной отправки должен быть редирект
+        self.assertEqual(response.status_code, 302)
+        # Проверяем, что объект создался в БД
+        self.assertEqual(Feedback.objects.count(), 1)
